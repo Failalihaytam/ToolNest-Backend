9b03a10a5037191895d11667a22c5c90b27495f9
Haytham <003haytam2@gmail.com>
2025-05-25 02:31:12 +0100

    p

--- config/cors.php
+++ config/cors.php
@@ -4,7 +4,7 @@ return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
     'allowed_origins' => ['http://localhost:3000'],
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => ['/^https:\/\/toolnest.*$/i'],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
